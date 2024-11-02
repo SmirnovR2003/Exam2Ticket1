3e585558f28a72bd24f5f2c021b16ede204c23b0
xratq <44581405+xratq@users.noreply.github.com>
2024-11-02 11:47:39 +0300

    task1

---
+++ local/templates/furniture_blue/components/bitrix/news/.default/.parameters.php
@@ -0,0 +1,10 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arTemplateParameters = array(
+	"SET_SPECIALDATE" => Array(
+        "PARENT" => "BASE",
+		"NAME" => GetMessage("SET_SPECIALDATE"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+);
---
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.list/.default/component_epilog.php
@@ -0,0 +1,4 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+if(isset($arResult["SET_SPECIALDATE"]))
+    $APPLICATION->SetPageProperty("specialdate", $arResult["SET_SPECIALDATE"]);
